46661e92f1c2218208fde7b147f5714d005e66dc
lmbsog0 <guillaume.sottas@liebherr.com>
2020-03-19 18:06:53 +0100

    add test for multiple val_table nodes

--- test_grammar.py
+++ test_grammar.py
@@ -48,13 +48,25 @@ def value_table_description(double, char_string):
 @pytest.mark.parametrize('value_descriptions_count', [1, 2])
 @pytest_cases.parametrize_plus('value_description_0', [pytest_cases.fixture_ref('value_table_description')])
 @pytest_cases.parametrize_plus('value_description_1', [pytest_cases.fixture_ref('value_table_description')])
-def value_tables(value_table_name, value_descriptions_count, value_description_0, value_description_1):
+def value_table(value_table_name, value_descriptions_count, value_description_0, value_description_1):
     value_description_0_string, value_description_0_value = value_description_0
     value_description_1_string, value_description_1_value = value_description_1
     value_descriptions_string = [value_description_0_string, value_description_1_string][:value_descriptions_count]
     value_descriptions_value = [value_description_0_value, value_description_1_value][:value_descriptions_count]
     return ('VAL_TABLE_ {} {} ;'.format(value_table_name, ' '.join(value_descriptions_string)),
             ValTable(value_table_name, value_descriptions_value))
+
+
+@pytest_cases.fixture_plus()
+@pytest.mark.parametrize('value_tables_count', [1, 2])
+@pytest_cases.parametrize_plus('value_table_0', [pytest_cases.fixture_ref('value_table')])
+@pytest_cases.parametrize_plus('value_table_1', [pytest_cases.fixture_ref('value_table')])
+def value_tables(value_tables_count, value_table_0, value_table_1):
+    value_table_0_string, value_table_0_value = value_table_0
+    value_table_1_string, value_table_1_value = value_table_1
+    value_tables_string = '\n'.join([value_table_0_string, value_table_1_string][:value_tables_count])
+    value_tables_value = [value_table_0_value, value_table_1_value][:value_tables_count]
+    return value_tables_string, value_tables_value
 
 
 @pytest.mark.parametrize('prop, value', (
@@ -118,9 +130,10 @@ def test_nodes_node(node_name_string_0, node_name_string_1):
     assert p.ast.nodes == ([node_name_string_0] if node_name_string_0 else []) + [node_name_string_1]
 
 
-@pytest_cases.parametrize_plus('value_table', [pytest_cases.fixture_ref('value_tables')])
-def test_value_tables_node(value_table):
-    value_table_string, value_table_value = value_table
+@pytest_cases.parametrize_plus('vts', [pytest_cases.fixture_ref('value_tables')])
+def test_value_tables_node(vts):
+    value_table_string, value_table_value = vts
     p = DbcParser(value_table_string)
     assert isinstance(p.ast.value_tables, list)
-    assert p.ast.value_tables[0] == value_table_value
+    for index, val_table in enumerate(value_table_value):
+        assert p.ast.value_tables[index] == val_table
