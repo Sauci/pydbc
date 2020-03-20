069e620d72bd0d087f2a333561a8dbb13f002c17
lmbsog0 <guillaume.sottas@liebherr.com>
2020-03-20 08:17:22 +0100

    add tests for signals node

--- test_grammar.py
+++ test_grammar.py
@@ -34,6 +34,8 @@ new_symbols_strings = ['CM_',
                        'BU_SG_REL_',
                        'BU_EV_REL_',
                        'BU_BO_REL_']
+unsigned_integers = [0, 1, 2]
+multiplexer_indicators = [' ', 'M', 'm0']
 
 
 @pytest_cases.fixture_plus()
@@ -67,6 +69,104 @@ def value_tables(value_tables_count, value_table_0, value_table_1):
     value_tables_string = '\n'.join([value_table_0_string, value_table_1_string][:value_tables_count])
     value_tables_value = [value_table_0_value, value_table_1_value][:value_tables_count]
     return value_tables_string, value_tables_value
+
+
+@pytest_cases.fixture_plus()
+@pytest.mark.parametrize('receivers_count', [1, 2])
+@pytest.mark.parametrize('receiver_0', c_identifiers + ['Vector_XXX'])
+@pytest.mark.parametrize('receiver_1', c_identifiers + ['Vector_XXX'])
+def receivers(receivers_count, receiver_0, receiver_1):
+    receivers_string = ', '.join([receiver_0, receiver_1][:receivers_count])
+    receivers_value = [receiver_0, receiver_1][:receivers_count]
+    return receivers_string, receivers_value
+
+
+@pytest_cases.fixture_plus()
+@pytest.mark.parametrize('signal_name', c_identifiers)
+@pytest.mark.parametrize('multiplexer_indicator', multiplexer_indicators)
+@pytest.mark.parametrize('start_bit', unsigned_integers)
+@pytest.mark.parametrize('signal_size', unsigned_integers)
+@pytest.mark.parametrize('byte_order', ['0', '1'])
+@pytest.mark.parametrize('value_type', ['+', '-'])
+@pytest.mark.parametrize('factor', doubles)
+@pytest.mark.parametrize('offset', doubles)
+@pytest.mark.parametrize('minimum', doubles)
+@pytest.mark.parametrize('maximum', doubles)
+@pytest.mark.parametrize('unit', char_strings)
+@pytest_cases.parametrize_plus('rs', [pytest_cases.fixture_ref('receivers')])
+def signal(signal_name,
+           multiplexer_indicator,
+           start_bit,
+           signal_size,
+           byte_order,
+           value_type,
+           factor,
+           offset,
+           minimum,
+           maximum,
+           unit,
+           rs):
+    receivers_string, receivers_value = rs
+    return ('SG_ {} {} : {} | {} @ {} {} ({}, {}) [ {} | {} ] {} {}'.format(signal_name,
+                                                                            multiplexer_indicator,
+                                                                            start_bit,
+                                                                            signal_size,
+                                                                            byte_order,
+                                                                            value_type,
+                                                                            factor,
+                                                                            offset,
+                                                                            minimum,
+                                                                            maximum,
+                                                                            unit,
+                                                                            receivers_string),
+            Signal(signal_name,
+                   multiplexer_indicator,
+                   start_bit,
+                   signal_size,
+                   byte_order,
+                   value_type,
+                   factor,
+                   offset,
+                   minimum,
+                   maximum,
+                   unit,
+                   receivers_value))
+
+
+@pytest_cases.fixture_plus()
+@pytest.mark.parametrize('signals_count', [1, 2])
+@pytest_cases.parametrize_plus('signal_0', [pytest_cases.fixture_ref('signal')])
+@pytest_cases.parametrize_plus('signal_1', [pytest_cases.fixture_ref('signal')])
+def signals(signals_count, signal_0, signal_1):
+    signal_0_string, signal_0_value = signal_0
+    signal_1_string, signal_1_value = signal_1
+    signals_string = '\n'.join([signal_0_string, signal_1_string][:signals_count])
+    signals_value = [signal_0_value, signal_1_value][:signals_count]
+    return signals_string, signals_value
+
+
+@pytest_cases.fixture_plus()
+@pytest.mark.parametrize('message_id', unsigned_integers)
+@pytest.mark.parametrize('message_name', c_identifiers)
+@pytest.mark.parametrize('message_size', unsigned_integers)
+@pytest.mark.parametrize('transmitter', c_identifiers + ['Vector_XXX'])
+@pytest_cases.parametrize_plus('ss', [pytest_cases.fixture_ref('signals')])
+def message(message_id, message_name, message_size, transmitter, ss):
+    signals_string, signals_value = ss
+    return ('BO_ {} {} : {} {} {}'.format(message_id, message_name, message_size, transmitter, signals_string),
+            Message(message_id, message_name, message_size, transmitter, signals_value))
+
+
+@pytest_cases.fixture_plus()
+@pytest.mark.parametrize('messages_count', [1, 2])
+@pytest_cases.parametrize_plus('message_0', [pytest_cases.fixture_ref('message')])
+@pytest_cases.parametrize_plus('message_1', [pytest_cases.fixture_ref('message')])
+def messages(messages_count, message_0, message_1):
+    message_0_string, message_0_value = message_0
+    message_1_string, message_1_value = message_1
+    messages_string = '\n'.join([message_0_string, message_1_string][:messages_count])
+    messages_value = [message_0_value, message_1_value][:messages_count]
+    return messages_string, messages_value
 
 
 @pytest.mark.parametrize('prop, value', (
@@ -137,3 +237,12 @@ def test_value_tables_node(vts):
     assert isinstance(p.ast.value_tables, list)
     for index, val_table in enumerate(value_table_value):
         assert p.ast.value_tables[index] == val_table
+
+
+@pytest_cases.parametrize_plus('ms', [pytest_cases.fixture_ref('messages')])
+def test_signal_node(ms):
+    messages_string, messages_value = ms
+    p = DbcParser(messages_string)
+    assert isinstance(p.ast.messages, list)
+    for index, m in enumerate(messages_value):
+        assert p.ast.messages[index] == m
